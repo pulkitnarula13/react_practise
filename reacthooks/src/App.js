f5eaab7983575a06c5360f9fc60afb97a1f12146
Pulkit Narula <43206958+pulkitnarula13@users.noreply.github.com>
2022-08-10 15:56:15 -0700

    Added the functions in reacthooks

--- reacthooks/src/App.js
+++ reacthooks/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  // useState and hooks can be used in functiononly not in Class
+  // useState must be on the Top of the function
+
+  const [count, setCount] = useState(4)
+
+
+  function decrementCount() {
+    setCount(prevCount => prevCount-1)
+  }
+
+  function incrementCount() {
+    setCount(count + 1)
+  }
+
   return (
     <>
-      <button>-</button>
-      <span>0</span>
-      <button>+</button>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
     </>
   );
 }
